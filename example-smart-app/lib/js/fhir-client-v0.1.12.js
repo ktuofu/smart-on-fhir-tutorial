93e488552213afe60c8b18caed16452893c49e1e
ktuofu <115111098+ktuofu@users.noreply.github.com>
2022-10-05 14:25:48 -0600

    Update fhir-client-v0.1.12.js

--- example-smart-app/lib/js/fhir-client-v0.1.12.js
+++ example-smart-app/lib/js/fhir-client-v0.1.12.js
@@ -12744,7 +12744,7 @@ var
 	cssShow = { position: "absolute", visibility: "hidden", display: "block" },
 	cssNormalTransform = {
 		letterSpacing: "0",
-		fontWeight: "400"
+		fontWeight: "100"
 	},
 
 	cssPrefixes = [ "Webkit", "O", "Moz", "ms" ];
